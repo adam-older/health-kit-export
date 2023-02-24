e5b1d18246d317a6dbd3e8b53f569d8077b2a36f
Adam Older <adamolder10@gmail.com>
2023-02-23 18:16:39 -0800

    Initial Commit

---
+++ HealthKitExport/HealthKitExportApp.swift
@@ -0,0 +1,17 @@
+//
+//  HealthKitExportApp.swift
+//  HealthKitExport
+//
+//  Created by Adam Older on 2/23/23.
+//
+
+import SwiftUI
+
+@main
+struct HealthKitExportApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
